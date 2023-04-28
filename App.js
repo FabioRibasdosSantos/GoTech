1d5a248ba59f53b0f17a8bdc8bd8853a1ef5610e
Fabio Ribas <fabyo_27@hotmail.com>
2023-04-28 14:14:32 -0300

    atualização commit

--- App.js
+++ App.js
@@ -1,35 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { Alert, StyleSheet, View, Image, TextInput,TouchableOpacity } from 'react-native';
+import React, { Component } from 'react';
+import { Alert, Text, StyleSheet, View, Image, TextInput,TouchableOpacity } from 'react-native';
 
-export default function App() {
+export default class App extends Component() {
 
+  clicou = () => {
+    Alert.alert('Go Tech', "Você está logado!")
+  }
 
+render() {
   return (
     <View style={styles.container}>
-      <Image source={require('./assets/logo.png')} style={styles.logo} />
+
+      <Image 
+      source={require('./assets/logo.png')}
+       style={styles.logo} 
+       />
       
       <TextInput
-        placeholder="Digite seu usuário"
-        secureTextEntry={true}
         style={styles.input}
+        placeholder="Digite seu usuário"
       />
 
       <TextInput
-        placeholder="Digite sua senha"
         style={styles.input}
+        secureTextEntry={true}
+        placeholder="Digite sua senha"
       />
+
+      <TouchableOpacity
+      style={styles.botao}
+      onPress={ () => {this.clicou()} }
+      >
+        <Text style={styles.botaoText}>Login</Text>
+      </TouchableOpacity>
 
       <StatusBar style="auto" />
     </View>
   );
 }
+}
+  
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#0f0f0f0',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#2c3e50'
   },
   logo: {
     width: 150,
@@ -40,9 +57,23 @@ const styles = StyleSheet.create({
     margemTop: 10,
     padding: 10,
     width: 300,
+    backgroundColor: '#fff',
     fontsize: 16,
     fontWeight: 'bold',
     borderRadius: 3
   },
-  
+  botao: {
+    width: 300,
+    height: 42,
+    backgroundColor: '#3498db',
+    marginTop: 10,
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  botaoText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#fff'
+  }
 });
